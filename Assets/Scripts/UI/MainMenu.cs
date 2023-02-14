0a697a5b31c37e2c34482a069b4e3edaa24ca8aa
DevilDrinksTea <mw.chapman82@hotmail.com>
2023-02-14 11:38:31 +0000

    Added main menu and scenes

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for main menu Canvas
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Method to exit game
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/StageSelector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Class for switching to different levels(scenes) in the game
+/// </summary>
+public class StageSelector : MonoBehaviour
+{
+    //Methods to change to different levels
+    public void Mainlevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    //Extra stuff if we want to do other modes
+
+    /*
+    public void HardMode()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void FreeMovementMode()
+    {
+        SceneManager.LoadScene(3);
+    }
+    */
+}
